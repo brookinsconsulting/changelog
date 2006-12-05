d50752509a28829a8ee98d34e10bb07a5c7ba172
Kristof Coomans <kristof.coomans@telenet.be>
2006-12-05 13:10:06 +0000

    extension info file for eZ publish 3.9

---
+++ ezinfo.php
@@ -0,0 +1,15 @@
+<?php
+class changelogInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "changelog",
+            'Version' => "",
+            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>
