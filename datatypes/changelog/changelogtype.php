ddee61f4b4de8e0d3da15ee903a811e377f34226
Brookins Consulting <info@brookinsconsulting.com>
2012-03-27 06:53:52 -0500

    Updated: Replaced ezi18n calls with ezpI18n::tr to be compatible with ez 4.3+ (4.4/4.7/CB2012.02).

--- datatypes/changelog/changelogtype.php
+++ datatypes/changelog/changelogtype.php
@@ -4,7 +4,7 @@ class ChangelogType extends eZTextType
 {
     function ChangelogType()
     {
-        $this->eZDataType( 'changelog', ezi18n( 'kernel/classes/datatypes', 'Changelog', 'Datatype name' ),
+        $this->eZDataType( 'changelog', ezpI18n::tr( 'kernel/classes/datatypes', 'Changelog', 'Datatype name' ),
                            array( 'serialize_supported' => true,
                                   'object_serialize_map' => array( 'data_text' => 'text' ) ) );
     }
@@ -28,7 +28,7 @@ class ChangelogType extends eZTextType
                 {
                     // not the edited language
                     eZDebug::writeDebug( 'not the edited language', 'changelog::initializeObjectAttribute()' );
-                    $dataText = ezi18n( 'kernel/classes/datatypes', '- copy of %version', '', array( '%version' => $originalContentObjectAttribute->attribute( 'version' ) ) );
+                    $dataText = ezpI18n::tr( 'kernel/classes/datatypes', '- copy of %version', '', array( '%version' => $originalContentObjectAttribute->attribute( 'version' ) ) );
                 }
                 else
                 {
@@ -36,7 +36,7 @@ class ChangelogType extends eZTextType
                     eZDebug::writeDebug( 'the edited language', 'changelog::initializeObjectAttribute()' );
                     eZDebug::writeDebug( $objectVersion->attribute( 'status' ), 'changelog::initializeObjectAttribute() original version status' );
 
-                    $dataText = ezi18n( 'kernel/classes/datatypes', '- based on version %version', '', array( '%version' => $originalContentObjectAttribute->attribute( 'version' ) ) );
+                    $dataText = ezpI18n::tr( 'kernel/classes/datatypes', '- based on version %version', '', array( '%version' => $originalContentObjectAttribute->attribute( 'version' ) ) );
 
                     $doNotCopyChangelog = array( eZContentObjectVersion::STATUS_PUBLISHED,
                                                  eZContentObjectVersion::STATUS_ARCHIVED );
@@ -55,7 +55,7 @@ class ChangelogType extends eZTextType
                 {
                     // translation to a new language
                     eZDebug::writeDebug( 'translation to a new language', 'changelog::initializeObjectAttribute()' );
-                    $dataText = ezi18n( 'kernel/classes/datatypes', '- translation based on %version', '', array( '%version' => $originalContentObjectAttribute->attribute( 'version' ) ) );
+                    $dataText = ezpI18n::tr( 'kernel/classes/datatypes', '- translation based on %version', '', array( '%version' => $originalContentObjectAttribute->attribute( 'version' ) ) );
 
                     $contentObjectAttribute->setAttribute( 'data_text', $dataText );
                 }
